47f9ae20cfdd911c6fbf3c8790d89a802348472d
Márkó Jaksa <jaksamarkodaniel@gmail.com>
2022-06-09 14:27:06 +0200

    Add fucking server compile

--- dist/server/server.js
+++ dist/server/server.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const http_1 = __importDefault(require("http"));
 const compression_1 = __importDefault(require("compression"));
-const port = 8080;
+const port = process.env.PORT || 8080;
 class App {
     constructor(port) {
         this.port = port;
@@ -17,6 +17,18 @@ class App {
         // In the webpack version of the boilerplate, it is not necessary
         // to add static references to the libs in node_modules if
         // you are using module specifiers in your client.ts imports.
+        //
+        // Visit https://sbcode.net/threejs/module-specifiers/ for info about module specifiers
+        //
+        // This server.ts is only useful if you are running this on a production server or you
+        // want to see how the production version of bundle.js works
+        //
+        // to use this server.ts
+        // # npm run build        (this creates the production version of bundle.js and places it in ./dist/client/)
+        // # tsc -p ./src/server  (this compiles ./src/server/server.ts into ./dist/server/server.js)
+        // # npm start            (this starts nodejs with express and serves the ./dist/client folder)
+        //
+        // visit http://127.0.0.1:3000
         this.server = new http_1.default.Server(app);
     }
     Start() {
